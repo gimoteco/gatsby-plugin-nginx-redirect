5cfd0f7e21a288cb4df1a39133989c3e5731cd96
Guilherme Ferreira <guilherme.ferreira@x-apps.com.br>
2020-05-22 16:59:37 -0400

    fix: rewrite regex

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,28 +1,31 @@
 import { NginxConfFile } from "nginx-conf";
+import { remove } from "fs-extra";
 
 export async function onPostBuild({ store, reporter }, options) {
   const { redirects } = store.getState();
 
-  NginxConfFile.create(options.inputConfigFile, function (err, conf) {
+  NginxConfFile.create(options.inputConfigFile, async function (err, conf) {
     if (err) {
       console.log(err);
       return;
     }
+
     conf.die(options.inputConfigFile);
     conf.flush();
 
     redirects.forEach((redirect) => {
       conf.nginx.server._add(
         "rewrite",
-        `^${redirect.fromPath}$ ^${redirect.toPath}$ ${
+        `^${redirect.fromPath}$ ${redirect.toPath} ${
           redirect.isPermanent ? "permanent" : "redirect"
         }`
       );
     });
 
+    await remove(options.outputConfigFile);
     conf.live(options.outputConfigFile);
     conf.flush();
   });
 
-  reporter.warn(`Redirects ${JSON.stringify(redirects)}`);
+  reporter.warn(`Added redirects to ${options.outputConfigFile}`);
 }
